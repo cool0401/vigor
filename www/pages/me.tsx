21385cdd72d869b57c34e90d3cbee9a5572da94d
root <root@DESKTOP-89TG73S>
2024-04-08 16:08:47 +0200

    init commit

---
+++ www/pages/me.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from "react";
+import Layout from "../components/Layout";
+import { useAuth } from "../auth";
+
+const Me: FunctionComponent = (): React.ReactElement => {
+  const { loading, user } = useAuth();
+  if (loading) {
+    return (
+      <Layout>
+        <p>Loading...</p>
+      </Layout>
+    );
+  } else {
+    return (
+      <Layout>
+        <h1 className="text-xl pt-3 pb-5">Welcome {user.username}!</h1>
+        <span>Profile details:</span>
+        <ul className="mt-4">
+          <li>ID: {user.id}</li>
+          <li>Email: {user.email}</li>
+          <li>Username: {user.username}</li>
+        </ul>
+      </Layout>
+    );
+  }
+}
+
+export default Me;
