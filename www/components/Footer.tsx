7e97eb0feff1eb210af23efa72dbdbd4139980ea
cool0401 <david.simon0205@gmail.com>
2024-04-08 18:53:11 +0200

    define footer

---
+++ www/components/Footer.tsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+import Image from 'next/image'
+
+const Footer = (): React.ReactElement => {
+  return (
+    <div className="bg-black p-20 mx-auto flex flex-col items-center">
+        
+        <Image
+            src="/images/logo.png"
+            width={300}
+            alt="Logo"
+            height={300}
+            className="pb-16"
+        />
+
+        <div className="w-full block flex-grow lg:flex lg:items-center border-b-2 border-gray-700 pb-4">
+            <div className="text-sm lg:flex-grow flex justify-center">
+            <Link href="#home" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white ml-6 mr-6">
+                Home
+                </a>
+            </Link>
+            <Link href="#services" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white mr-6">
+                Services
+                </a>
+            </Link>
+            <Link href="#resources" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white mr-6">
+                Resources
+                </a>
+            </Link>
+            <Link href="#investments" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white mr-6">
+                Investments
+                </a>
+            </Link>
+            <Link href="#reviews" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white mr-6">
+                Reviews
+                </a>
+            </Link>
+            <Link href="#contactus" legacyBehavior>
+                <a className="block mt-6 lg:inline-block lg:mt-0 text-white text-lg hover:text-white mr-6">
+                Contact Us
+                </a>
+            </Link>
+            </div>
+        </div>
+
+        <h1 className="text-gray-500 text-xl mt-8">Copyright Reserved</h1>
+    </div>
+  )
+}
+
+export default Footer;
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -1,10 +1,12 @@
 import Layout from "../components/Layout";
 import Home from "../components/Home";
+import Footer from "../components/Footer";
 
 const HomePage = (): React.ReactElement => {
   return (
     <Layout>
       <Home/>
+      <Footer />
     </Layout>
   )
 }
